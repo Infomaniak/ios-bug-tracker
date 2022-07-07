6db8d7637e39c4f4ceb3f4a339de5399d6798a97
Florentin Bekier <florentin.bekier@infomaniak.com>
2022-07-07 11:21:37 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# InfomaniakBugTracker
+
+A description of this package.
---
+++ Sources/InfomaniakBugTracker/InfomaniakBugTracker.swift
@@ -0,0 +1,6 @@
+public struct InfomaniakBugTracker {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}

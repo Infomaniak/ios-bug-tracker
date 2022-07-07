4bb01fd067be3e63cd4f1b37aec61614bf73d048
Florentin Bekier <florentin.bekier@infomaniak.com>
2022-07-07 11:25:52 +0200

    Set minimal iOS version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "InfomaniakBugTracker",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -24,5 +25,4 @@ let package = Package(
         .testTarget(
             name: "InfomaniakBugTrackerTests",
             dependencies: ["InfomaniakBugTracker"]),
-    ]
-)
+    ])

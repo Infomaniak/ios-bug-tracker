005daba38fb677116d3db3ae37ba539ba10b2bee
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2025-09-18 15:07:34 +0200

    chore: Bump Core

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/Infomaniak/ios-core", .upToNextMajor(from: "16.0.0")),
-        .package(url: "https://github.com/Infomaniak/ios-version-checker", .upToNextMajor(from: "14.0.0")),
+        .package(url: "https://github.com/Infomaniak/ios-core", .upToNextMajor(from: "17.0.0")),
+        .package(url: "https://github.com/Infomaniak/ios-version-checker", .upToNextMajor(from: "15.0.0")),
     ],
     targets: [
         .target(
